566dd1ca5921d7be8c3be66746afc3a34c6f010a
Kurt <kurt.pac@gmail.com>
2017-03-02 15:27:49 +1100

    Object3D and Meshes
    Added Object3D and all of it's children (Scene, Model, Mesh, Camera).
    Also added the ability to load a .mesh file that contains mesh graphic
    data.

--- AGE.js
+++ AGE.js
@@ -1,12 +1,364 @@
 var AGE = AGE || (function()
 {
     var gl = null;
+    var frame = null;
+
+    var scenes = {};
+    var models = {};
+    var meshes = {};
+    var textures = {};
 
     var shaderPrograms = {};
     var programsLoading = 0;
     var onProgramsLoaded = null;
 
-    /////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////
+
+    var Vector = {};
+
+    /**
+     * Either adds scalar u to vector v, or adds vector u to vector v where vector u
+     * and v are of the same length.
+     *
+     * @param u {number|number[]}
+     * @param v {number[]}
+     * @returns {number[]}
+     */
+    Vector.add = function(u, v)
+    {
+        var i;
+
+        if(typeof u == "number") {
+            for(i = 0; i < v.length; i++) v[i] += u;
+            return v;
+        }
+
+        if(u.length != v.length)
+            throw new VectorException("Both vectors must be of equal length.");
+
+        for(i = 0; i < u.length; i++) u[i] += v[i];
+        return u;
+    };
+
+    /**
+     * Subtracts u from v, see Vector.add().
+     *
+     * @param u {number|number[]}
+     * @param v {number[]}
+     * @returns {number[]}
+     */
+    Vector.subtract = function(u, v)
+    {
+        var i;
+
+        if(typeof u == "number") {
+            for(i = 0; i < v.length; i++) v[i] -= u;
+            return v;
+        }
+
+        if(u.length != v.length)
+            throw new VectorException("Both vectors must be of equal length.");
+
+        for(i = 0; i < u.length; i++) v[i] -= u[i];
+        return u;
+    };
+
+    /**
+     * Dot product of vectors u and v.
+     *
+     * @param u {number[]}
+     * @param v {number[]}
+     * @returns {number}
+     */
+    Vector.dot = function(u, v)
+    {
+        if(u.length != v.length)
+            throw new VectorException("u and v must be the same length.");
+
+        var result = 0;
+
+        for(var i = 0; i < u.length; i++) result += u[i] * v[i];
+
+        return result;
+    };
+
+    /**
+     * Cross product of 3-dimensional vectors u and v.
+     *
+     * @param u {number[]}
+     * @param v {number[]}
+     */
+    Vector.cross = function(u, v)
+    {
+        if(u.length != 3 || v.length != 3)
+            throw new VectorException("u and v must be of length 3 to compute the cross product.");
+
+        var i = u[1] * v[2] - u[2] * v[1];
+        var j = -1 * (u[0] * v[2] - u[2] * v[0]);
+        var k = u[0] * v[1] - u[1] * v[0];
+
+        return [i, j, k];
+    };
+
+    function VectorException(message)
+    {
+        this.name = "VectorException";
+        this.message = message;
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////
+
+    var Matrix = {};
+
+    /**
+     * Alias for Vector.add().
+     *
+     * @param u {number|number[]}
+     * @param v {number[]}
+     * @returns {number[]}
+     */
+    Matrix.add = function(u, v)
+    {
+        try {
+            return Vector.add(u, v);
+        }
+        catch(e) {
+            throw e;
+        }
+    };
+
+    /**
+     * Alias for Vector.subtract().
+     *
+     * @param u {number|number[]}
+     * @param v {number[]}
+     * @returns {number[]}
+     */
+    Matrix.subtract = function(u, v)
+    {
+        try {
+            return Vector.subtract(u, v);
+        }
+        catch(e) {
+            throw e;
+        }
+    };
+
+    /**
+     * Multiplies u by v if both are matrices, or v by u if u is a scalar. If uColumns
+     * if not given then u is assumed to be a square matrix (if not scalar).
+     *
+     * @param u {number|number[]}
+     * @param v {number[]}
+     * @param [uColumns] {number}.
+     */
+    Matrix.multiply = function(u, v, uColumns)
+    {
+        if(typeof u == "number") {
+            for(var i = 0; i < v.length; i++) {
+                v[i] *= u;
+            }
+            return v;
+        }
+
+        if(typeof uColumns == "undefined") {
+            uColumns = Math.sqrt(u.length);
+            if(uColumns != parseInt(uColumns))
+                throw new MatrixException("u must be square if uColumns is undefined.");
+            if(v.length % uColumns != 0)
+                throw new MatrixException("v has the wrong size to multiply with u.");
+        }
+
+        var uRows = u.length / uColumns;
+        if(uRows != parseInt(uColumns))
+            throw new MatrixException("uColumns does not match u size.");
+
+        var vColumns = v.length / uColumns;
+
+        var uIndex, vIndex, mIndex = 0;
+        var m = [];
+
+        for(var ur = 0; ur < uRows; ur++) {
+            for(var vc = 0; vc < vColumns; vc++) {
+                m[mIndex] = 0;
+                for(var uc = 0; uc < uColumns; uc++) {
+                    uIndex = ur * uColumns + uc;
+                    vIndex = uc * vColumns + vc;
+                    m[mIndex] += u[uIndex] * v[vIndex];
+                }
+                mIndex++;
+            }
+        }
+
+        return m;
+    };
+
+    /**
+     * Creates an identity matrix with rows and columns equaling size.
+     *
+     * @param size {number}
+     * @returns {number[]}
+     */
+    Matrix.createIdentity = function(size)
+    {
+        var identity = [];
+        var spacing = size + 1;
+
+        for(var i = 0; i < Math.pow(size, 2); i++) {
+            if(i % spacing == 0) identity[i] = 1;
+            else identity[i] = 0;
+        }
+
+        return identity;
+    };
+
+    /**
+     * Creates a square matrix of zeros with rows and columns equaling size.
+     *
+     * @param size {number}
+     * @returns {number[]}
+     */
+    Matrix.createZeros = function(size)
+    {
+        var matrix = [];
+        for(var i = 0; i < Math.pow(size, 2); i++) {
+            matrix[i] = 0;
+        }
+
+        return matrix;
+    };
+
+    function MatrixException(message)
+    {
+        this.name = "MatrixException";
+        this.message = message;
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////
+
+    function Attribute(type, size, data)
+    {
+        this.size = size;
+
+        switch(type) {
+            case "float32" :
+                data = new Float32Array(data);
+                break;
+            default:
+                data = [];
+                console.error("Unknown attribute type \"" + type + "\".");
+                console.warn("An empty data set was loaded to attribute buffer.");
+        }
+
+        this.buffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+        gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////
+
+    function Frame() {}
+
+    //////////////////////////////////////////////////////////////////////////////////
+
+    function Object3D()
+    {
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+        this.xScale = 1;
+        this.yScale = 1;
+        this.zScale = 1;
+        this.matrix = Matrix.createIdentity(4);
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////
+
+    Scene.prototype = new Object3D();
+    Scene.prototype.constructor = Scene;
+
+    function Scene()
+    {
+        Object3D.call(this);
+    }
+
+    Scene.prototype.addModel = function(name) {};
+
+    Scene.prototype.removeModel = function(model) {};
+
+
+    //////////////////////////////////////////////////////////////////////////////////
+
+    Camera.prototype = new Object3D();
+    Camera.prototype.constructor = Camera;
+
+    function Camera()
+    {
+        Object3D.call(this);
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////
+
+    Model.prototype = new Object3D();
+    Model.prototype.constructor = Model;
+
+    function Model()
+    {
+        Object3D.call(this);
+    }
+
+    Model.prototype.addMesh = function(name) {};
+
+    Model.prototype.removeMesh = function(mesh) {};
+
+    //////////////////////////////////////////////////////////////////////////////////
+
+    Mesh.prototype = new Object3D();
+    Mesh.prototype.constructor = Mesh;
+
+    function Mesh(src)
+    {
+        Object3D.call(this);
+        this.loaded = false;
+        this.onLoad = null;
+        this.src = src;
+        this.shaderPrograms = [];
+        this.textures = [];
+        this.indices = null;
+        this.attributes = {};
+    }
+
+    Mesh.prototype.load = function(callback)
+    {
+        var mesh = this;
+
+        getJSON(this.src, function(data)
+        {
+            mesh.shaderPrograms = data.shaderPrograms;
+
+            mesh.indices = gl.createBuffer();
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, mesh.indices);
+            gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(data.indices), gl.STATIC_DRAW);
+
+            var i;
+
+            for(i = 0; i < data.attributes.length; i++) {
+                var attribute = data.attributes[i];
+                mesh.attributes[attribute.name] = new Attribute(attribute.type, attribute.size, attribute.data);
+            }
+
+            for(i = 0; i < data.textures.length; i++) {
+                mesh.textures.push(data.textures[i].name);
+                addTexture(data.textures[i].name, data.textures[i].src);
+            }
+
+            mesh.loaded = true;
+            if(typeof mesh.onLoad === "function") mesh.onLoad();
+            if(typeof callback === "function") callback();
+        });
+    };
+
+    //////////////////////////////////////////////////////////////////////////////////
 
     function ShaderProgram(vertShader, fragShader)
     {
@@ -52,8 +404,9 @@ var AGE = AGE || (function()
         gl.attachShader(this.program, this.fragmentShader.shader);
         gl.linkProgram(this.program);
 
+
         if (!gl.getProgramParameter(this.program, gl.LINK_STATUS)) {
-            console.log("Unable to initialize the shader program: " + gl.getProgramInfoLog(this.program));
+            console.error("[Program Compile Error] " + gl.getProgramInfoLog(this.program));
             return false;
         }
 
@@ -101,9 +454,11 @@ var AGE = AGE || (function()
         gl.shaderSource(this.shader, this.file);
         gl.compileShader(this.shader);
 
+        delete this.file;
+
         if (!gl.getShaderParameter(this.shader, gl.COMPILE_STATUS)) {
+            console.error("[Shader Compile Error] " + gl.getShaderInfoLog(this.shader));
             gl.deleteShader(this.shader);
-            console.log("An error occurred compiling the shaders: " + gl.getShaderInfoLog(this.shader));
             return false;
         }
 
@@ -133,6 +488,10 @@ var AGE = AGE || (function()
 
     //////////////////////////////////////////////////////////////////////////////////
 
+    function Texture() {}
+
+    //////////////////////////////////////////////////////////////////////////////////
+
     function getFile(src, callback)
     {
         if(typeof callback !== "function") return;
@@ -141,8 +500,14 @@ var AGE = AGE || (function()
 
         xhttp.onreadystatechange = function()
         {
-            if (this.readyState == 4 && this.status == 200)
-               callback(this.responseText);
+            if (this.readyState == 4) {
+                switch(this.status) {
+                    case 200:
+                        callback(this.responseText);
+                        break;
+                }
+            }
+
         };
 
         xhttp.open("GET", src);
@@ -160,7 +525,7 @@ var AGE = AGE || (function()
         })
     }
 
-    addShaderProgram = function(src)
+    function addShaderProgram(src)
     {
         programsLoading++;
 
@@ -187,7 +552,7 @@ var AGE = AGE || (function()
             if(--programsLoading == 0 && typeof onProgramsLoaded === "function")
                 onProgramsLoaded();
         });
-    };
+    }
 
     function compileShaderPrograms()
     {
@@ -202,8 +567,11 @@ var AGE = AGE || (function()
                 shaderPrograms[name].load();
             }
         }
+    }
 
-        console.log(shaderPrograms);
+    function addTexture(name, src)
+    {
+
     }
 
     //////////////////////////////////////////////////////////////////////////////////
@@ -212,11 +580,7 @@ var AGE = AGE || (function()
         bootstrap : function(canvas)
         {
             gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-
-            if (!gl) {
-                console.log("Unable to initialize WebGL.");
-                return;
-            }
+            if (!gl) return;
 
             gl.clearColor(0, 0, 0, 1);         // Clear to black, fully opaque
             gl.clearDepth(1);                  // Clear everything
@@ -224,11 +588,6 @@ var AGE = AGE || (function()
             gl.depthFunc(gl.LEQUAL);           // Near things obscure far things
             gl.enable(gl.CULL_FACE);
             gl.cullFace(gl.BACK);
-
-            onShaderProgramsCompiled = function()
-            {
-
-            };
 
             if(programsLoading > 0) {
                 onProgramsLoaded = function () {
@@ -261,13 +620,30 @@ var AGE = AGE || (function()
             getJSON(src, function(file)
             {
                 for(var i = 0; i < file.length; i++) {
-                    console.log(directory + file[i]);
                     addShaderProgram(directory + file[i]);
                 }
 
                 programsLoading--;
             });
-        }
+        },
+
+        addMesh : function(name, src)
+        {
+            var mesh = new Mesh(src);
+            meshes[name] = mesh;
+            mesh.load(function()
+            {
+                console.log(mesh);
+                console.log(meshes);
+            });
+            console.log(meshes);
+        },
+
+        newScene : function(name) {},
+
+        setScene : function(name) {},
+
+        newModel : function(name) {}
     };
 
     //////////////////////////////////////////////////////////////////////////////////
